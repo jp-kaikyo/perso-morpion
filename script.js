18d1b8cd1d14f3c776ddbf80fc7594e4a6db079b
joao paulo <joaopaulopintoqueiros4pgmail.com>
2025-09-10 03:31:35 +0200

    MISE au point du js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-//// Variables globales ////
+//// variable ////
 
 
 let joueurActuel = "X";
 let scoreX = 0;
 let scoreO = 0;
+
+
+
+
+//// Attendre que HTML et CSS soient chargés ////
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Page chargée !');
+});
